malexmave/nfcgate

commit 17ffc0f51f2859742411942cbd2275eac204fd02

Improved dl_iterate_phdr usage
* MapInfo now uses base address from dl_iterate_phdr instead of looking up
  the base again
* Refactored range lookups to use new common data structure
* Code cleanup

Co-Authored-By: Steffen Klee <sklee@seemoo.tu-darmstadt.de>

nfcd/src/main/jni/include/nfcd/helper/MapInfo.h
@@ -3,24 +3,41 @@
 
 #include <set>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class MapInfo {
 public:
     struct RangeData {
-        uint64_t start, end;
+        RangeData(uintptr_t s, uintptr_t e, uint8_t p) : start(s), end(e), perms(p) { }
+
+        uintptr_t start, end;
         uint8_t perms;
+    };
+    struct LibraryData {
+        explicit LibraryData(uintptr_t b, const std::string &l) : base(b), label(l) { }
+
+        uintptr_t base;
         std::string label;
+        std::vector<RangeData> ranges;
+    };
+    struct LookupResult {
+        operator bool() const { // NOLINT(*-explicit-constructor)
+            return library && range;
+        }
+
+        const LibraryData *library;
+        const RangeData *range;
     };
 
     bool create();
 
     std::set<std::string> loadedLibraries() const;
     void *getBaseAddress(const std::string &library) const;
-    const RangeData *rangeFromAddress(uintptr_t addr, uint64_t size = 0) const;
+    LookupResult lookupRange(uintptr_t address, uint64_t size = 0) const;
 
 protected:
-    std::vector<RangeData> mRanges;
+    std::unordered_map<std::string, LibraryData> mLibraryData;
 };
 
 #endif //NFCD_MAPINFO_H

nfcd/src/main/jni/src/helper/MapInfo.cpp
@@ -10,13 +10,17 @@ bool MapInfo::create() {
     int rv = dl_iterate_phdr([] (struct dl_phdr_info *info, size_t, void *user_data) {
         auto *instance = (MapInfo *)user_data;
 
+        // map library name to new library entry with base address
+        auto &entry = *instance->mLibraryData.try_emplace(info->dlpi_name, info->dlpi_addr, info->dlpi_name).first;
         for (size_t i = 0; i < info->dlpi_phnum; i++) {
-            instance->mRanges.push_back(RangeData {
-                info->dlpi_addr + info->dlpi_phdr[i].p_vaddr,
-                info->dlpi_addr + info->dlpi_phdr[i].p_vaddr + info->dlpi_phdr[i].p_memsz,
-                static_cast<uint8_t>(info->dlpi_phdr[i].p_flags),
-                info->dlpi_name
-            });
+            entry.second.ranges.emplace_back(
+                    // range start address
+                    info->dlpi_addr + info->dlpi_phdr[i].p_vaddr,
+                    // range end address
+                    info->dlpi_addr + info->dlpi_phdr[i].p_vaddr + info->dlpi_phdr[i].p_memsz,
+                    // range permissions
+                    static_cast<uint8_t>(info->dlpi_phdr[i].p_flags)
+            );
         }
 
         return 0;
@@ -29,37 +33,25 @@ bool MapInfo::create() {
 std::set<std::string> MapInfo::loadedLibraries() const {
     std::set<std::string> result;
 
-    for (auto &range : mRanges) {
-        result.emplace(range.label);
-    }
+    for (auto &it : mLibraryData)
+        result.emplace(it.first);
 
     return result;
 }
 
 void *MapInfo::getBaseAddress(const std::string &library) const {
-    for (auto &range : mRanges) {
-        // skip range that does not match the library
-        if (!StringUtil::strEndsWith(range.label, library))
-            continue;
-
-        // skip range without read permission or without enough space for ELF header
-        if ((range.perms & 4) != 4 || range.end - range.start <= 4)
-            continue;
-
-        // check ELF magic bytes to confirm this region as the base
-        if (memcmp((void *)range.start, "\x7f" "ELF", 4) != 0)
-            continue;
-
-        return (void *)range.start;
-    }
+    auto it = mLibraryData.find(library);
+    if (it != mLibraryData.end())
+        return reinterpret_cast<void*>(it->second.base);
 
     return nullptr;
 }
 
-const MapInfo::RangeData *MapInfo::rangeFromAddress(uintptr_t addr, uint64_t size) const {
-    for (auto &range : mRanges)
-        if (addr >= range.start && (addr + size) <= range.end)
-            return &range;
+MapInfo::LookupResult MapInfo::lookupRange(uintptr_t address, uint64_t size) const {
+    for (auto &it : mLibraryData)
+        for (auto &range : it.second.ranges)
+            if (address >= range.start && (address + size) <= range.end)
+                return {&it.second, &range};
 
-    return nullptr;
+    return {};
 }

nfcd/src/main/jni/src/helper/ELFMemoryParser.h
@@ -206,7 +206,7 @@ protected:
     }
 
     // current base address
-    uint8_t *mBase;
+    uint8_t *mBase = nullptr;
     // lowest address of all PT_LOADs to relocate against
     size_t mLowestLoad = 0;
 

nfcd/src/main/jni/src/nfcd.cpp
@@ -222,13 +222,13 @@ bool HookGlobals::checkNFACBOffset(uint32_t offset) const {
     auto **p_nfa_conn_cback = (def_NFA_CONN_CBACK**)(nfa_dm_cb->address<uint8_t>() + offset);
     LOG_ASSERT_S(*p_nfa_conn_cback, return false, "p_conn_cback is null, offset may be invalid");
 
-    auto rangeInfo = mapInfo.rangeFromAddress(reinterpret_cast<uintptr_t>(*p_nfa_conn_cback));
-    LOG_ASSERT_S(rangeInfo, return false, "p_conn_cback range info invalid");
+    auto lookup = mapInfo.lookupRange(reinterpret_cast<uintptr_t>(*p_nfa_conn_cback));
+    LOG_ASSERT_S(lookup, return false, "p_conn_cback lookup invalid");
     LOGD("checkOffset: candidate p_conn_cback %p with permissions %d in object file %s",
-         *p_nfa_conn_cback, rangeInfo->perms, rangeInfo->label.c_str());
-    LOG_ASSERT_S((rangeInfo->perms & 1) == 1, return false,
+         *p_nfa_conn_cback, lookup.range->perms, lookup.library->label.c_str());
+    LOG_ASSERT_S((lookup.range->perms & 1) == 1, return false,
                  "p_conn_cback permissions not execute, offset likely invalid");
-    LOG_ASSERT_S(StringUtil::strContains(StringUtil::toLower(rangeInfo->label), "jni"), return false,
+    LOG_ASSERT_S(StringUtil::strContains(StringUtil::toLower(lookup.library->label), "jni"), return false,
                  "p_conn_cback not in JNI object, offset likely invalid");
 
     LOGD("checkOffset: success");

commit ceb81b76b969415da397214af5837fb124948b5f

Fix library data base address for non-PIE/PIC
dlpi_addr contains the relocation address. To obtain the base address we
need to add the PT_LOAD p_vaddr.
This only fails for non-PIE/-PIC since p_vaddr is 0 for PIE/PIC.

Regression introduced by 17ffc0f51f2859742411942cbd2275eac204fd02.

Fixes gh-193

nfcd/src/main/jni/include/nfcd/helper/MapInfo.h
@@ -15,9 +15,9 @@ public:
         uint8_t perms;
     };
     struct LibraryData {
-        explicit LibraryData(uintptr_t b, const std::string &l) : base(b), label(l) { }
+        explicit LibraryData(const std::string &l) : label(l) { }
 
-        uintptr_t base;
+        uintptr_t base = 0;
         std::string label;
         std::vector<RangeData> ranges;
     };

nfcd/src/main/jni/src/helper/MapInfo.cpp
@@ -11,8 +11,13 @@ bool MapInfo::create() {
         auto *instance = (MapInfo *)user_data;
 
         // map library name to new library entry with base address
-        auto &entry = *instance->mLibraryData.try_emplace(info->dlpi_name, info->dlpi_addr, info->dlpi_name).first;
+        auto &entry = *instance->mLibraryData.try_emplace(info->dlpi_name, info->dlpi_name).first;
         for (size_t i = 0; i < info->dlpi_phnum; i++) {
+            // set base address to relocation + PT_LOAD vaddr if not already set
+            // PT_LOAD headers are sorted in ascending vaddr order so using the first is correct
+            if (info->dlpi_phdr[i].p_type == PT_LOAD && entry.second.base == 0)
+                entry.second.base = info->dlpi_addr + info->dlpi_phdr[i].p_vaddr;
+
             entry.second.ranges.emplace_back(
                     // range start address
                     info->dlpi_addr + info->dlpi_phdr[i].p_vaddr,